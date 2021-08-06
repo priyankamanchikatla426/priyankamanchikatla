fd1bd6a37771dc54c7083c31fc9b58344f67d6e0
priyankamanchikatla <51701567+priyankamanchikatla@users.noreply.github.com>
2021-08-06 07:46:59 +0100

    Update medical.spec.js

--- cypress/integration/medical-test/medical.spec.js
+++ cypress/integration/medical-test/medical.spec.js
@@ -1,17 +1,16 @@
 /// <reference types="cypress" />
 
-const { get } = require("cypress/types/lodash");
-
 describe('visit medical url', () => {
   beforeEach(() => {
     cy.visit('https://www.medicines.org.uk/emc/browse-companies');
+    cy.get('#onetrust-accept-btn-handler').click();
   })
 
   it('02- get companies details', () => {
 
     const companiesList = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","Z"];
 
-    pages.forEach(companiesList => {
+    companiesList.forEach(companiesList => {
 
       cy
         .contains(companiesList)
