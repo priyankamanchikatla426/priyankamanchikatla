2f02c5df6359f9b4e1998ec5d47200f1a6dd6152
priyankamanchikatla <51701567+priyankamanchikatla@users.noreply.github.com>
2021-08-06 17:07:09 +0100

    Update medical.spec.js

--- cypress/integration/medical-test/medical.spec.js
+++ cypress/integration/medical-test/medical.spec.js
@@ -1,21 +1,20 @@
 /// <reference types="cypress" />
 
-const { array } = require("assert-plus");
-
 describe('visit medical url', () => {
   beforeEach(() => {
     cy.visit('https://www.medicines.org.uk/emc/browse-companies');
     cy.get('#onetrust-accept-btn-handler').click();
   })
 
-  it('02- get companies details', () => {
-    //  const companiesList = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","Z"];
+  it('02- get companies count', () => {
+     const companiesList = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","Z"];
+     for( var i=0;i<companiesList.length;i++){
+    const countOfCompanies =  console.log(companiesList[i]);
+    cy.log(countOfCompanies);
+       cy.get('a[href*="/emc/browse-companies/companiesList[i]').click();
 
-    cy.get('a[href*="/emc/browse-companies/A').click();
-    cy.get('.:nth-child(2) > .col-md-12').children().click();
-    cy.go('back')
-    cy.get('.ieleft > ul > :nth-child(3) > .key').click();
+     }
+
+    })
   })
-
-})
 
