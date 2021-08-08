83004826663512fa6e27dc05e0785f8029db137f
priyankamanchikatla <51701567+priyankamanchikatla@users.noreply.github.com>
2021-08-08 19:15:12 +0100

    Update medical.spec.js

--- cypress/integration/medical-test/medical.spec.js
+++ cypress/integration/medical-test/medical.spec.js
@@ -6,12 +6,17 @@ describe('visit medical url', () => {
     cy.get('#onetrust-accept-btn-handler').click();
   })
 
-  it('02- get companies count', () => {
+  it('02- get companies list', () => {
      const companiesList = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","Z"];
      for( var i=0;i<companiesList.length;i++){
-    const countOfCompanies =  console.log(companiesList[i]);
-    cy.log(countOfCompanies);
-       cy.get('a[href*="/emc/browse-companies/companiesList[i]').click();
+     console.log(companiesList[i]);
+     cy.get('ul>li').each(($el, index, $list) => {
+      if ($el=== cy.get('.key').eq(1)) {
+        cy.wrap($el).click()
+      } else {
+        // do something else
+      }
+    })
 
      }
 
