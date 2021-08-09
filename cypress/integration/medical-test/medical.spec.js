ee3dc76bbcb3fb37dd3d2c36f791cb7421368adb
priyankamanchikatla <51701567+priyankamanchikatla@users.noreply.github.com>
2021-08-09 14:22:58 +0100

    Update medical.spec.js

--- cypress/integration/medical-test/medical.spec.js
+++ cypress/integration/medical-test/medical.spec.js
@@ -10,7 +10,7 @@ describe('visit medical url', () => {
      const companiesList = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","Z"];
      for( var i=0;i<companiesList.length;i++){
      console.log(companiesList[i]);
-
+     cy.get('.col-md-12').its('length');
     cy.get(':nth-child(1) > .key').click({multiple: true});
      }
 
